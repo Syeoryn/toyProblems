5132a76004abcd2caeda9085669c3b8d8d1a385e
Hack Reactor Students <accounts@hackreactor.com>
2014-04-17 01:29:47 -0700

    Implemented recursive fasterFib function

--- recursive_fun.js
+++ recursive_fun.js
@@ -12,3 +12,14 @@ function nFibonacci(n){
     return (nFibonacci(n-1) || 1) + (nFibonacci(n-2) || 0);
   }
 }
+
+// n fibonacci, but faster (use while-loop logic)
+function fasterFib(n,n1,n2){
+  if(n<2 && !n1) {return 1}
+  n1 = n1 || 1;
+  n2 = n2 || 1;
+  while(n > 2){
+    return fasterFib(--n,n1+n2,n1);
+  }
+  return n1+n2;
+}
