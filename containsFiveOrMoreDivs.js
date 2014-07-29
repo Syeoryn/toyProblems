0c8ac5bd29850dee5af950fb42c98a653b269e92
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-28 23:55:15 -0700

    Cleans code

--- containsFiveOrMoreDivs.js
+++ containsFiveOrMoreDivs.js
@@ -2,9 +2,7 @@ var containsFiveOrMoreDivs = function(node){
   node = node || document.body;
   var counter = 0;
   var countDivs = function(node){
-    if(node.tagName === 'DIV'){
-      counter++;
-    }
+    if(node.tagName === 'DIV') counter++;
     for(var i = 0; i < node.children.length; i++){
       countDivs(node.children[i]);
     }
@@ -34,15 +32,11 @@ var fiveOrMoreDivs = function(node, depth){
   node = node || document.body;
   depth = depth || 0;
   var divCount = 0;
-  if(node.tagName === 'DIV'){
-    divCount++;
-  }
+  if(node.tagName === 'DIV') divCount++;
   for(var i = 0; i < node.children.length; i++){
     divCount += fiveOrMoreDivs(node.children[i], depth + 1);
   }
-  if(depth === 0){
-    return divCount >= 5;
-  }
+  if(depth === 0) return divCount >= 5;
   return divCount;
 }
 
@@ -52,20 +46,12 @@ var efficientFiveOrMoreDivs = function(node, depth, divCount){
   node = node || document.body;
   depth = depth || 0;
   divCount = divCount || 0;
-  if(node.tagName === 'DIV'){
-    divCount++;
-  }
+  if(node.tagName === 'DIV') divCount++;
   for(var i = 0; i < node.children.length; i++){
     var count = efficientFiveOrMoreDivs(node.children[i], depth + 1, divCount);
-    if(count === true){
-      return true;
-    }
-    if(count !== true){
-      divCount = count;
-    }
+    if(count === true) return true;
+    if(count !== true) divCount = count;
   }
-  if(depth === 0){
-    return divCount >= 5;
-  }
+  if(depth === 0) return divCount >= 5;
   return divCount >= 5 || divCount;
 }
