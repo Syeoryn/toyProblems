0c0e3151994c23aff560dd22dd29e04a41d0b484
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-28 23:17:33 -0700

    Implements fiveOrMoreDivs with no inner helper function

--- containsFiveOrMoreDivs.js
+++ containsFiveOrMoreDivs.js
@@ -12,3 +12,20 @@ var containsFiveOrMoreDivs = function(node){
   countDivs(node);
   return counter >= 5;
 }
+
+// same function, with no inner helper function
+var fiveOrMoreDivs = function(node, depth){
+  node = node || document.body;
+  depth = depth || 0;
+  var divCount = 0;
+  if(node.tagName === 'DIV'){
+    divCount++;
+  }
+  for(var i = 0; i < node.children.length; i++){
+    divCount += fiveOrMoreDivs(node.children[i], depth + 1);
+  }
+  if(depth === 0){
+    return divCount >= 5;
+  }
+  return divCount;
+}
