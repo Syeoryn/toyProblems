778f4fbecb3dfa60ba19534cb1e630f914b77171
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-28 23:01:32 -0700

    Implements containsFiveOrMoreDivs with an inner function

---
+++ containsFiveOrMoreDivs.js
@@ -0,0 +1,16 @@
+var containsFiveOrMoreDivs = function(node){
+  node = node || document.body;
+  var counter = 0;
+  var countDivs = function(node){
+    if(node.tagName === 'DIV'){
+      counter++;
+    }
+    if(node.children){
+      for(var i = 0; i < node.children.length; i++){
+        countDivs(node.children[i]);
+      }
+    }
+  }
+  countDivs(node);
+  return counter >= 5;
+}
