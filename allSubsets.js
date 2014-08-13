cb1a09470479d0485c060aafd3b50f512170db3e
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-13 14:44:45 -0700

    Describes allSubsets

---
+++ allSubsets.js
@@ -0,0 +1,8 @@
+// Write a method that returns a list of all subsets of a set.
+// Assume that all items in a set are unique
+// example: set: 'abc'
+//          subsets: ['a','b','c','ab','ac','bc']
+
+var allSubsets = function(set){
+
+}
