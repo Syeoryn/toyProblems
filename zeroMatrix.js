3bf8463d4b35c8d48416bac4dfd7cb7a78dca291
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-12 22:28:41 -0700

    Implements zeroMatrix.js

--- zeroMatrix.js
+++ zeroMatrix.js
@@ -2,5 +2,29 @@
 // its entire row and column are set to 0
 
 var zeroMatrix = function(matrix){
-  
+  var zeroRows = {};
+  var zeroColumns = {};
+
+  for(var row = 0; row < matrix.length; row ++){
+    for(var column = 0; column < matrix[0].length; column++){
+      if(matrix[row][column] === 0){
+        zeroRows[row] = true;
+        zeroColumns[column] = true;
+      }
+    }
+  }
+
+  for(row in zeroRows){
+    for(var i = 0; i < matrix.length; i++){
+      matrix[row][i] = 0;
+    }
+  }
+
+  for(column in zeroColumns){
+    for(var i = 0; i < matrix[0].length; i++){
+      matrix[i][column] = 0;
+    }
+  }
+
+  return matrix;
 }
