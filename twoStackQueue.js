c2bb6362c4d1c49766ce0a2ebc954501c14885e3
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-12 23:38:47 -0700

    Explains pseudoclassical vs functional instantiation pattern

--- twoStackQueue.js
+++ twoStackQueue.js
@@ -1,5 +1,15 @@
 // Implement a queue using two stacks
 
+
+// Queue is a constructor function (to be invoked with the new keyword).
+// The pseudoclassical instantiation pattern in javascript uses this 
+// constructor to instantiate new instances of an object/ class.
+// In this instantiation pattern, each instance of a class refers to
+// the same method object-- the prototype property of the constructor
+// This saves space by not creating new helper functions in memory,
+// but does not allow for private variables due to the use of the 
+// this keyword
+
 var Queue = function(){
   // keep inbox and outbox stacks to maintain constant time complexity
   this._inbox = [];
@@ -31,6 +41,9 @@ Queue.prototype.remove = function(){
 /* Now implemet a queue with two stacks,
    but maintain the inbox and outbox as private variables */
 
+// must use functional instantiation to create private variables in javascript
+// this means that rather than a constructor, a factory function is used, 
+// which creates new instances of each helper function for each queue that's instantiated
 var queueMaker = function(){
 
 }
