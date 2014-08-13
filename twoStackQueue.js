171b29e0b44589316028fa3aa3116a34d2b5cb01
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-12 23:13:51 -0700

    Implements Queue

--- twoStackQueue.js
+++ twoStackQueue.js
@@ -1,5 +1,20 @@
 // Implement a queue using two stacks
 
 var Queue = function(){
+  this._inbox = [];
+  this._outbox = [];
+}
 
+Queue.prototype.add = function(value){
+  this._inbox.push(value);
+  return value;
+}
+
+Queue.prototype.remove = function(){
+  if(!this._outbox.length){
+    while(this._inbox.length){
+      this._outbox.push(this._inbox.pop());
+    }
+  }
+  return this._outbox.pop();
 }
