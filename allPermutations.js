1295898e3b8fdbb344c311bac19446ac69c98457
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-14 14:08:12 -0700

    Completes allPermutations

--- allPermutations.js
+++ allPermutations.js
@@ -2,6 +2,15 @@
 // example: string-- 'abc'
 //          permutations-- ['abc','acb','bac','bca','cab','cba']
 
-var allPermutations = function(string){
+var allPermutations = function(string, permutations, current){
+  var permutations = permutations || [];
+  var current = current || '';
 
+  if(!string) permutations.push(current);
+
+  for(var i = 0; i < string.length; i++){
+    allPermutations(string.slice(0,i) + string.slice(i+1), permutations, current + string[i]);
+  }
+
+  return permutations;
 }
