3858598b8319ea5d230bb9a34f1e7772e91dd3c5
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-14 14:02:34 -0700

    Describes allPermutations

---
+++ allPermutations.js
@@ -0,0 +1,7 @@
+// Write a function that returns a list of all permutations of a string
+// example: string-- 'abc'
+//          permutations-- ['abc','acb','bac','bca','cab','cba']
+
+var allPermutations = function(string){
+
+}
