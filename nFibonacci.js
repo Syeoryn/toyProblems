6be69484ab32455b0632f8392d8d54dc53a28def
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-15 02:53:41 -0700

    Completes nFibonacci

--- nFibonacci.js
+++ nFibonacci.js
@@ -1,5 +1,10 @@
 // Write a function that computes the nth fibonacci number
 
-var nFibonacci = function(num){
-
+var nFibonacci = function(n, x, prev){
+  x = x || 1;
+  if(x === Infinity) return Infinity;
+  if(n > 1){
+    return nFibonacci(n - 1, x + prev|| 0, x);
+  }
+  return x;
 }
