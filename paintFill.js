bceb98d1b6e3404bf89042e5496a1525a9d8a689
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-16 13:18:12 -0700

    Describes paintFill

---
+++ paintFill.js
@@ -0,0 +1,8 @@
+// Implement the 'paint fill' function in image editing functions.
+// Given an image (two dimensional array), a specific point
+// within it, and a specific color, fill the surrounding area
+// until the entire area is the new color
+
+var paintFill = function(){
+
+}
