d1352a618259f27eda33bb124c8b8e95938a9ae4
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-16 14:58:47 -0700

    Completes paintFill

--- paintFill.js
+++ paintFill.js
@@ -3,6 +3,33 @@
 // within it, and a specific color, fill the surrounding area
 // until the entire area is the new color
 
-var paintFill = function(){
+var paintFill = function(image, newColor, origin, originColor){
+  originColor = originColor || image[origin[0]][origin[1]];
+  // change the originColor to newColor
+  image[origin[0]][origin[1]] = newColor;
+  // Then, starting from the origin:
+  // if above is initial origin color
+  if(image[origin[0] + 1][origin[1]] === originColor){
+    // recurse on that point
+    paintFill(image, newColor, [origin[0] + 1, origin[1]], originColor);
+  }
 
+  // if right is initial origin color
+  if(image[origin[0]][origin[1] + 1] === originColor){
+    // recurse on that point
+    paintFill(image, newColor, [origin[0], origin[1] + 1], originColor);
+  }
+
+  // if below is initial origin color
+  if(image[origin[0] - 1][origin[1]] === originColor){
+    // recurse on that point
+    paintFill(image, newColor, [origin[0] - 1, origin[1]], originColor);
+  }
+
+  // if left is initial origin color
+  if(image[origin[0]][origin[1] - 1] === originColor){
+    // recurse on that point
+    paintFill(image, newColor, [origin[0], origin[1] - 1], originColor);
+  }
+  return image;
 }
