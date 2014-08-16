6aca435cb0e57b2a67b8e6641f39c5430628e801
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-16 13:13:39 -0700

    Completes magicIndex

--- magicIndex.js
+++ magicIndex.js
@@ -3,5 +3,15 @@
 // array with distinct values
 
 var findMagicIndex = function(array){
-
+  // store the magic indices in an array
+  var magicIndices = [];
+  // since the input array is sorted, only check as long as 
+  // array[i] <= i.  If this condition is ever false, then
+  // it will necessarily continue to be false in a sorted array
+  for(var i = 0; array[i] <= i; i++){
+    // add each magic index to storage
+    if(i === array[i]) magicIndices.push(i);
+  }
+  // return the storage array
+  return magicIndices;
 }
