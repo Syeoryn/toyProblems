a43f57bfe9e6c43958dc94d5649666724bc8ed6f
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-16 13:06:05 -0700

    Describes magicIndex

---
+++ magicIndex.js
@@ -0,0 +1,7 @@
+// A magic index is an array index where i === array[i]
+// Write a function to find all magic indices of a sorted
+// array with distinct values
+
+var findMagicIndex = function(array){
+
+}
