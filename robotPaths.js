e3b286802ded30866fca0c305db239cb50868efe
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-17 10:12:40 -0700

    Describes easyRobotPaths

---
+++ robotPaths.js
@@ -0,0 +1,7 @@
+// Imagine a robot is sitting in the upper left corner of an M x N board.
+// moving only right or down, how many paths could the robot take to get
+// to the bottom right corner?
+
+var easyRobotPaths = function(){
+
+}
