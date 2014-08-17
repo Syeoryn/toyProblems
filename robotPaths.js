d4c5d0018cefb3d0ff2b544503c12d3c4eb0397a
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-17 11:02:53 -0700

    Completes easyRobotPaths

--- robotPaths.js
+++ robotPaths.js
@@ -2,6 +2,29 @@
 // moving only right or down, how many paths could the robot take to get
 // to the bottom right corner?
 
-var easyRobotPaths = function(){
+var easyRobotPaths = function(m, n, x, y){
+  n = n || m;
+  x = x || 0;
+  y = y || 0;
+  var count = 0;
 
+  // if robot is at lower right corner
+  if(x === m - 1 && y === n - 1){
+    // increment count
+    count++;
+  }
+
+  // if robot can move right
+  if(x < m){
+    // move right
+    count += easyRobotPaths(m, n, x + 1, y);
+  }
+
+  // if robot can move down
+  if(y < n){
+    // move down
+    count += easyRobotPaths(m, n, x, y + 1);
+  }
+
+  return count;
 }
