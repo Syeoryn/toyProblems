9386f7352e38c7f3a87e1befc2c9c27dad167dab
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-19 16:45:46 -0700

    Describes ladderClimb

---
+++ ladderClimb.js
@@ -0,0 +1,7 @@
+// Imagine you have a ladder with n rungs.  You can go up the ladder
+// either 1, 2, or 3 rungs at a time.  How many possible paths are
+// there to get to the top rung?
+
+var ladderClimb = function(n){
+
+}
