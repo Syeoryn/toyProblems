70d70925e157f9eeab72ea4dc52e635434cd1c0e
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-19 17:34:16 -0700

    Completes ladderClimb

--- ladderClimb.js
+++ ladderClimb.js
@@ -2,6 +2,14 @@
 // either 1, 2, or 3 rungs at a time.  How many possible paths are
 // there to get to the top rung?
 
-var ladderClimb = function(n){
+var ladderClimb = function(n, currentRung){
+  var count = 0;
+  currentRung = currentRung || 0;
+  if(currentRung === n) return 1;
 
+  for(var i = 3; i > 0; i--){
+    if(n - currentRung >= i) count += ladderClimb(n, currentRung + i);
+  }
+
+  return count;
 }
